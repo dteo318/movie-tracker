cf430b63f725cb4ab88fb25babb1d4fc53e7502c
Daniel Teo <dteo318@gmail.com>
2021-05-09 21:14:45 -0700

    added models for watched movies

--- movie_tracker/movie_tracker/settings.py
+++ movie_tracker/movie_tracker/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
--- movie_tracker/tracker/models.py
+++ movie_tracker/tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    overview = models.CharField(max_length=500)
+    movie_id = models.IntegerField()
+    release_date = models.CharField(max_length=10)
+    runtime = models.IntegerField()
+
+    def __str__(self):
+        return self.title
